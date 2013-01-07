6168debc1d75ea548ebcd823c58cd17b3b792f97
Aidar <aidgab@yandex.ru>
2013-01-07 18:53:22 +0400

    Chat working

--- client.js
+++ client.js
@@ -1,13 +1,30 @@
 /**
  * Created with JetBrains PhpStorm.
- * User: Developer
+ * User: Adiar
  * Date: 07.01.13
  * Time: 17:42
- * To change this template use File | Settings | File Templates.
+ * Client script
  */
 
-var socket = io.connect('http://localhost:8088');
-socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
+
+$(document).ready(function (){
+    var socket = io.connect('http://localhost:8088');
+
+    socket.on('connected', function (data) {
+        console.log('Connected to socket.io');
+    });
+
+    socket.on('chat_msg', function (data) {
+        $('<div class="chat_msg_wrapper"><b>'+data.sender_name+': </b>'+data.text+'</div>').prependTo("#chatMsgHolder").effect("highlight", {}, 2000);
+    });
+
+    //Binding UI controls
+
+    $("#chatMsgSendBtn").click(function (e){
+        e.preventDefault();
+        socket.emit('chat_msg',{text: $("#chatMsgText").val()});
+        $("#chatMsgText").val('');
+        return false;
+    });
+
 });
--- server.js
+++ server.js
@@ -3,18 +3,22 @@
  * User: Developer
  * Date: 07.01.13
  * Time: 17:37
- * To change this template use File | Settings | File Templates.
+ * Basic server for broadcasts using node.js + socket.io
  */
-var app = require('http').createServer(handler)
-    , io = require('socket.io').listen(app)
-
-app.listen(8088);
+var io = require('socket.io').listen(8088);
 
 io.sockets.on('connection', function (socket) {
-    var roomId='broadcast1'
-    socket.join(roomId);
+    var roomId='broadcast1';
 
-    socket.on('my other event', function (data) {
-        console.log(data);
+    socket.join(roomId);
+    socket.emit('connected',null);
+
+    socket.on('chat_msg', function (data) {
+
+        //@todo strip_tags needed
+        io.sockets.in(roomId).emit('chat_msg',{
+            sender_name:    socket.id,
+            text:           data.text
+        });
     });
 });
